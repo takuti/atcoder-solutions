30b735b7af862452aa3474008a5ab728f0095043
takuti <k.takuti@gmail.com>
2015-07-09 00:03:50 +0900

    goodbye many if-statements

--- ABC/001/C.py
+++ ABC/001/C.py
@@ -20,80 +20,30 @@ def ans(Deg, Dis):
   """
 
   Dis = int(round(Dis * 10/ 60., 0))
-  if Dis <= 2: W = 0
-  elif Dis <= 15: W = 1
-  elif Dis <= 33: W = 2
-  elif Dis <= 54: W = 3
-  elif Dis <= 79: W = 4
-  elif Dis <= 107: W = 5
-  elif Dis <= 138: W = 6
-  elif Dis <= 171: W = 7
-  elif Dis <= 207: W = 8
-  elif Dis <= 244: W = 9
-  elif Dis <= 284: W = 10
-  elif Dis <= 326: W = 11
-  else: W = 12
+  Dis_th = [2, 15, 33, 54, 79, 107, 138, 171, 207, 244, 284, 326]
+  for i in range(13):
+    if i == 12:
+      W = 12
+    elif Dis <= Dis_th[i]:
+      W = i
+      break
 
   if W == 0:
     Dir = 'C'
+
   else:
     Deg = Deg / 10.
-    if Deg < 11.25 or Deg >= 348.75: Dir = 'N'
-    elif Deg < 33.75: Dir = 'NNE'
-    elif Deg < 56.25: Dir = 'NE'
-    elif Deg < 78.75: Dir = 'ENE'
-    elif Deg < 101.25: Dir = 'E'
-    elif Deg < 123.75: Dir = 'ESE'
-    elif Deg < 146.25: Dir = 'SE'
-    elif Deg < 168.75: Dir = 'SSE'
-    elif Deg < 191.25: Dir = 'S'
-    elif Deg < 213.75: Dir = 'SSW'
-    elif Deg < 236.25: Dir = 'SW'
-    elif Deg < 258.75: Dir = 'WSW'
-    elif Deg < 281.25: Dir = 'W'
-    elif Deg < 303.75: Dir = 'WNW'
-    elif Deg < 326.25: Dir = 'NW'
-    else: Dir = 'NNW'
+    directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']
 
-  print Dir, W
+    step = 22.5
+    upper = 11.25
+    i = 0
+    while upper < 360:
+      if Deg < upper: break
+      upper += step
+      i += 1
 
-def main():
-  Deg, Dis = map(int, raw_input().split(' '))
-  Dis = int(round(Dis * 10/ 60., 0))
-  if Dis <= 2: W = 0
-  elif Dis <= 15: W = 1
-  elif Dis <= 33: W = 2
-  elif Dis <= 54: W = 3
-  elif Dis <= 79: W = 4
-  elif Dis <= 107: W = 5
-  elif Dis <= 138: W = 6
-  elif Dis <= 171: W = 7
-  elif Dis <= 207: W = 8
-  elif Dis <= 244: W = 9
-  elif Dis <= 284: W = 10
-  elif Dis <= 326: W = 11
-  else: W = 12
-
-  if W == 0:
-    Dir = 'C'
-  else:
-    Deg = Deg / 10.
-    if Deg < 11.25 or Deg >= 348.75: Dir = 'N'
-    elif Deg < 33.75: Dir = 'NNE'
-    elif Deg < 56.25: Dir = 'NE'
-    elif Deg < 78.75: Dir = 'ENE'
-    elif Deg < 101.25: Dir = 'E'
-    elif Deg < 123.75: Dir = 'ESE'
-    elif Deg < 146.25: Dir = 'SE'
-    elif Deg < 168.75: Dir = 'SSE'
-    elif Deg < 191.25: Dir = 'S'
-    elif Deg < 213.75: Dir = 'SSW'
-    elif Deg < 236.25: Dir = 'SW'
-    elif Deg < 258.75: Dir = 'WSW'
-    elif Deg < 281.25: Dir = 'W'
-    elif Deg < 303.75: Dir = 'WNW'
-    elif Deg < 326.25: Dir = 'NW'
-    else: Dir = 'NNW'
+    Dir = directions[i]
 
   print Dir, W
 
